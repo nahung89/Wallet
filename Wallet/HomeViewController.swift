dd3e7f1003437b191182b591778208be986dc33c
Nah <nahung89@gmail.com>
2018-10-09 17:14:08 +0700

    1st init files

---
+++ Wallet/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeController.swift
+//  Wallet
+//
+//  Created by Nah on 10/8/18.
+//  Copyright © 2018 Nah. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet private var tableView: UITableView!
+    
+    
+}
